46a99b4150c7ea69cf851a9095ef41cf114c777c
Zane Hitchcox <zwhitchcox@gmail.com>
2020-08-19 15:54:46 -0400

    Update display-link.sh

--- display-link.sh
+++ display-link.sh
@@ -1,6 +1,7 @@
 # https://www.displaylink.com/downloads/file?id=1576
 # https://www.displaylink.com/downloads/file?d=310
 unzip *.zip
-chmod +x *.run
-sudo *.run
+chmod +x ./displaylink-driver-5.3.1.34.run
+sudo displaylink-driver-5.3.1.34.run
+cd DisplayLinkXServerPatch
 sudo dpkg -i xserver-xorg-core_1.20.8-2ubuntu2_amd64.deb
