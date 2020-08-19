1aac0b3a4f8224e5388edaa99bcb8a2117ce3d7c
Zane Hitchcox <zwhitchcox@gmail.com>
2020-08-19 16:15:18 -0400

    Update setup.sh

--- subpkg/vscode/setup.sh
+++ subpkg/vscode/setup.sh
@@ -8,4 +8,7 @@ sudo snap install --classic code
 fi
 
 
-sh "$HOME/zetup-pkg-ubuntu/subpkg/vscode/import-extensions.sh"
+ZETUP_DIR=$HOME/zetup-pkg-ubuntu
+sh "$ZETUP_DIR/subpkg/vscode/import-extensions.sh"
+cp "$ZETUP_DIR/subpkg/vscode/config/keybindings.json" "$HOME/.config/Code/User"
+cp "$ZETUP_DIR/subpkg/vscode/config/settings.json" "$HOME/.config/Code/User"
