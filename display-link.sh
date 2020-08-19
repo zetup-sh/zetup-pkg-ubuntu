e0288a70daf4e0b75b875cac7ef79050f4a979cd
Zane Hitchcox <zwhitchcox@gmail.com>
2020-08-19 15:51:47 -0400

    Update display-link.sh

--- display-link.sh
+++ display-link.sh
@@ -1,7 +1,5 @@
-# 
-
-curl -L -O "http://assets.displaylink.com/live/downloads/software/f1576_DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu%205.3.1.zip?AWSAccessKeyId=AKIAJHGQWPVXWHEDJUEA&Expires=1597860538&Signature=rHCCE9lfslaTmTw9KNoRxH3h4hQ%3D"
-curl -L -O "http://assets.displaylink.com/live/downloads/software/f1597_DisplayLink%20XServer%20Patch%2020.04.zip?AWSAccessKeyId=AKIAJHGQWPVXWHEDJUEA&Expires=1597861215&Signature=795H%2Ba%2FIdki94Mg%2Bd%2F8iovr9ovs%3D"
+# https://www.displaylink.com/downloads/file?id=1576
+# https://www.displaylink.com/downloads/file?d=310
 unzip *.zip
 chmod +x *.run
 sudo *.run
